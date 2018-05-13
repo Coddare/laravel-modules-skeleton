b1a8137723f70e4f1479d9863935dbe0c7e4a7da
Frederico Martini <fredmalmeida@gmail.com>
2018-05-13 15:17:43 -0300

    updated welcome message, added Modules to .gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel-Modules <br>Skeleton
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    TODO: <br>
+                    Create README
                 </div>
             </div>
         </div>
